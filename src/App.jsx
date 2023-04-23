cd25fa32b3e6a3869cf9de7fbe3592ef74c46f07
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2023-04-23 19:11:48 -0400

    Función de filtro de búsqueda 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Spinner from "./components/Spinner";
 import GamesCard from "./components/GamesCard";
 import Pagination from "./components/Pagination";
 import Footer from "./components/Footer";
-
+import List from "./components/List";
 
 
 function App() {
@@ -18,16 +18,6 @@ function App() {
   /* Asignación de direciónes para juegos y categorias */
   const gamesPath = "http://mi.zapto.org:3000/api/games";
   const categoriesPath = "http://mi.zapto.org:3000/api/collection";
-  /* Variables para busqueda*/
-  const [inputText, setInputText] = useState("");
-
-  /* funcion para busqueda */
-  function inputHandler(e) {
-    //convert input text to lower case
-    var lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
-    print(lowerCase)
-  }
 
   useEffect(() => {
     const use = async () => {
@@ -39,6 +29,7 @@ function App() {
       if (games.status !== 200)
         return alert("Error cargando la informacion de juegos");
       setGames(games.data);
+      setGamesList(games.data)
       setCategories(categories.data);
       setIsLoading(false);
     };
@@ -66,10 +57,34 @@ function App() {
   const firstGamesIndex = lastGamesIndex - gamesPerPage;
   const gamesFinal = games.slice(firstGamesIndex, lastGamesIndex);
 
+    /* Variables para busqueda*/
+    const [gamesList, setGamesList ] = useState("");
+    const [inputText, setInputText] = useState("");
+    /* funcion para busqueda */
+    function inputHandler(e) {
+      //convert input text to lower case
+      var lowerCase = e.target.value.toLowerCase();
+      setInputText(lowerCase);
+      console.log(e.target.value)
+      
+      filtar(lowerCase)
+    }
+    /* funcion para filtrar busqueda*/
+    const filtar=(inputText)=>{
+      var resultado = gamesList.filter((game)=>{
+        if(game.title.toLowerCase().includes(inputText.toLowerCase)){
+          console.log(game.title)
+          return game
+        }
+        })
+        /* console.log("Gamelist:"+gamesList) */
+        setGamesList(resultado)
+    }
+
   return (
     <div>
       <div className="container mx-auto p-10">
-      <SearchInput categories={categories} handleFilter={handleFilter} inputHandler={inputHandler}/>
+      <SearchInput categories={categories} handleFilter={handleFilter} inputHandler={inputHandler} />
       {!!loading && <Spinner />}
       <GamesCard games={gamesFinal} />
       <Pagination
